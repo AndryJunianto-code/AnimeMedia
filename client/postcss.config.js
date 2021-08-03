82bd568878fb62a529e77c3d2c94fcc794a5b525
AndryJunianto1023 <69632552+AndryJunianto1023@users.noreply.github.com>
2021-08-03 13:07:52 +0800

    setup

---
+++ client/postcss.config.js
@@ -0,0 +1,6 @@
+const tailwind = require('tailwindcss')
+module.exports = {
+    plugins:[
+        tailwind('./tailwind.config.js'),require('autoprefixer')
+    ]
+}
---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './style/main.css'
+import AuthProvider from './auth/AuthProvider'
+import {BrowserRouter as Router} from 'react-router-dom'
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+    <AuthProvider>
+    <App />
+    </AuthProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
